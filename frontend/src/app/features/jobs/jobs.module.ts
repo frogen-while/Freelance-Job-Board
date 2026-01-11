9b6ee8734c8a3c15489ba09e522ab4a7e64356cf
dasterer <maksimkocubajlo@gmail.com>
2026-01-11 04:50:18 +0100

    chore: remove tests and unused code

--- frontend/src/app/features/jobs/jobs.module.ts
+++ frontend/src/app/features/jobs/jobs.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { JobsListComponent } from './jobs-list.component';
 import { JobDetailComponent } from './job-detail.component';
 
 @NgModule({
-  declarations: [JobsListComponent, JobDetailComponent],
+  declarations: [JobDetailComponent],
   imports: [
     CommonModule,
     FormsModule,
     RouterModule.forChild([
-      { path: '', component: JobsListComponent },
       { path: ':id', component: JobDetailComponent }
     ])
   ]
